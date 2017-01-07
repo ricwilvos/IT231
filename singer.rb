a2c10957aaf88aded542f3fe724df25a24b00b5f
Rich <ricwilvos@gmail.com>
2017-01-07 17:19:06 +0000

    first try

---
+++ singer.rb
@@ -0,0 +1,18 @@
+# to run use a command like: ruby frank.rb -p $PORT -o $IP
+
+require 'sinatra'
+
+get '/' do
+    
+    @title = 'Homage to Frank!'
+    
+    erb :index
+end
+
+get '/two' do
+    @title = 'My bestest page'
+    
+    erb :two
+    
+end
+
